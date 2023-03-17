14c75e56fe185df5870034f6fc39a01b10eeb4eb
OsamahKhOdeh <eng.osama.odeh@gmail.com>
2023-03-17 18:48:11 +0400

    auth and datasheets api

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 console.log("From api");
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "http://143.42.61.215:5000" });
 // http://143.42.61.215:5001/
 
 //export const createProduct = (newProduct) => API.post("/products", newProduct);
