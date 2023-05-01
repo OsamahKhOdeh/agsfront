4b5b47cfe9fed7a97e7d8baa63f8586c28c53a60
OsamahKhOdeh <eng.osama.odeh@gmail.com>
2023-05-01 11:15:36 +0400

    price 4 didgts

--- src/Components/Invoice/InvoiceTableRow.js
+++ src/Components/Invoice/InvoiceTableRow.js
@@ -19,59 +19,67 @@ const styles = StyleSheet.create({
     borderRightWidth: 1,
     textAlign: "right",
     paddingRight: 8,
-    height : "100%"  ,paddingTop : 4
+    height: "100%",
+    paddingTop: 4,
   },
   description: {
     width: "45%",
     textAlign: "left",
     borderRightColor: borderColor,
     borderRightWidth: 1,
-    paddingLeft: 8,height : "100%"  ,paddingTop : 2
+    paddingLeft: 8,
+    height: "100%",
+    paddingTop: 2,
   },
   qty: {
     width: "15%",
     borderRightColor: borderColor,
     borderRightWidth: 1,
     textAlign: "center",
-    paddingRight: 8,height : "100%"  ,paddingTop : 2
+    paddingRight: 8,
+    height: "100%",
+    paddingTop: 2,
   },
   price: {
     width: "20%",
     borderRightColor: borderColor,
     borderRightWidth: 1,
     textAlign: "right",
-    paddingRight: 8,height : "100%"  ,paddingTop : 2
+    paddingRight: 8,
+    height: "100%",
+    paddingTop: 2,
   },
   amount: {
     width: "15%",
     textAlign: "right",
-    paddingRight: 8,height : "100%"  ,paddingTop : 2
+    paddingRight: 8,
+    height: "100%",
+    paddingTop: 2,
   },
 });
 
-const InvoiceTableRow = ({ products, currency ,location, usdToAedRate}) => {
-  
-  
-console.log({ products, currency ,location, usdToAedRate});
-function calcPrice(item) {
-  let price =0;
-  if(location === "freezone" && currency === "AED"){
-    price = item.freezonePriceAED;
+const InvoiceTableRow = ({ products, currency, location, usdToAedRate }) => {
+  console.log({ products, currency, location, usdToAedRate });
+  function calcPrice(item) {
+    let price = 0;
+    if (location === "freezone" && currency === "AED") {
+      price = item.freezonePriceAED;
+    }
+    if (location === "local" && currency === "AED") {
+      price = item.LocalPriceAED;
+    }
+    if (location === "freezone" && currency === "USD") {
+      price = item.freezonePrice;
+    }
+    if (location === "local" && currency === "USD") {
+      price = item.LocalPrice;
+    }
+
+    return price;
   }
-  if(location === "local" && currency === "AED"){
-    price = item.LocalPriceAED;
-  }
-  if(location === "freezone" && currency === "USD"){
-    price = item.freezonePrice;
-  }if(location === "local" && currency === "USD"){
-    price = item.LocalPrice;
-  }
-  
-  return price;
-}
   let no = 0;
-  const rows = products.map((item , index) => (
-    <View style={styles.row}  key={index}>
+  const rows = products.map((item, index) => (
+    <View style={styles.row} key={index}>
       <Text style={styles.no}>{index + 1}</Text>
       <Text style={styles.description}>
         {item.brand}&nbsp;
@@ -79,12 +87,12 @@ function calcPrice(item) {
       </Text>
       <Text style={styles.qty}>{item.qty}</Text>
       <Text style={styles.price}>
-        {calcPrice(item)?.toFixed(3)}
-          &nbsp;&nbsp;
+        {calcPrice(item)?.toFixed(4)}
+        &nbsp;&nbsp;
         {currency}
       </Text>
       <Text style={styles.amount}>
-        {item.qty>0 ? (item.qty * calcPrice(item))?.toFixed(3) : 0}&nbsp;&nbsp;
+        {item.qty > 0 ? (item.qty * calcPrice(item))?.toFixed(2) : 0}&nbsp;&nbsp;
         {currency}
       </Text>
     </View>
